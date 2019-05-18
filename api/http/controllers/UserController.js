30b9da89bb0b565c612a2f3373887363e86aa979
Eran Machiels <info@eranmachiels.nl>
2019-05-18 11:47:17 +0200

    Added getting of user, for showoff

--- api/http/controllers/UserController.js
+++ api/http/controllers/UserController.js
@@ -1,11 +1,27 @@
 const Controller = require('./Controller');
+const UserRepository = require('./../../database/repositories/UserRepository');
 
 class UserController extends Controller {
 
-    index(world) {
-        return this.response.send(`Hello ${world}`);
+    /**
+     * @type {UserRepository}
+     */
+    userRepository;
+
+    constructor() {
+        super();
+
+        this.userRepository = new UserRepository();
+    }
+
+    async index() {
+        const user = await this.userRepository.find(1);
+
+        return this.response.send(user.getName());
     }
 
 }
 
 module.exports = UserController;
+
+
